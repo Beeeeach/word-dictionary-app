801327b31a1cd658aa7c7854d2f4d4506069d555
Beeeeach <beach_naoki@outlook.jp>
2026-09-06 21:09:32 +0900

    Delete lib directory

--- lib/actions/comments-fetch.ts
+++
@@ -1,8 +0,0 @@
-"use server";
-
-import { getCommentsForPost, type CommentWithAuthor } from "@/lib/data/comments";
-
-/** クライアントコンポーネントからコメント一覧を取得するためのラッパー */
-export async function getComments(postId: string): Promise<CommentWithAuthor[]> {
-  return getCommentsForPost(postId);
-}
